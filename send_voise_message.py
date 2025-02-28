781a5d0891f34a1f32885f6b3ab4065a8c6ce1f0
Ajayrajc1998 <ajayrajc007@gmail.com>
2025-02-28 17:45:08 +0530

    Rename task and DAG IDs for voice call operations in autofinix-reminder.py and send_voice_message.py

--- send_voise_message.py
+++ send_voise_message.py
@@ -37,7 +37,7 @@ BASE_STORAGE_DIR = "/appz/data"
 
 # Define DAG
 with DAG(
-    "send-voice-message",
+    "twilio_voice_call_direct",
     default_args=default_args,
     schedule_interval=None,  # Manually triggered
     catchup=False,
