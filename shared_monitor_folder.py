f874934036eb598e38bc5b439af4379387894afe
anugrahp0 <aprathap@ecloudcontrol.com>
2025-03-19 15:13:43 +0530

    added minute deg

--- shared_monitor_folder.py
+++ shared_monitor_folder.py
@@ -4,16 +4,15 @@ from airflow.utils.dates import days_ago
 
 # Default arguments for the DAG
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
+    "owner": "lowtouch.ai_developers",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 2, 27),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 # Simple function to print a message
-def say_hello():
-    print("Hello! This DAG runs every minute.")
+
 
 # DAG definition
 with DAG(
@@ -21,12 +20,14 @@ with DAG(
     default_args=default_args,
     description='A simple DAG that runs every minute',
     schedule_interval='* * * * *',  # Cron expression for every minute
-    start_date=days_ago(1),
     catchup=False,
 ) as dag:
+    def say_hello():
+        print("Hello! This DAG runs every minute.")
 
     # PythonOperator to run the simple function
     hello_task = PythonOperator(
         task_id='say_hello',
         python_callable=say_hello,
     )
+    hello_task
