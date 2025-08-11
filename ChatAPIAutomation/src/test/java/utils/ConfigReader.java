f06ebf1ce663dc3553f4ed80fb49a460bdd81d23
adhilcc <aahammed@ecloudcontrol.com>
2025-08-11 12:17:52 +0530

    test

--- ChatAPIAutomation/src/test/java/utils/ConfigReader.java
+++ ChatAPIAutomation/src/test/java/utils/ConfigReader.java
@@ -1,22 +1,12 @@
 package utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class ConfigReader {
-    private static final Properties properties = new Properties();
-
-    static {
-        try {
-            FileInputStream fis = new FileInputStream("src/test/resources/application.properties");
-            properties.load(fis);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load application.properties file", e);
-        }
-    }
 
     public static String get(String key) {
-        return properties.getProperty(key);
+        String value = System.getenv(key);
+        if (value == null) {
+            throw new RuntimeException("Environment variable '" + key + "' not found");
+        }
+        return value;
     }
 }
