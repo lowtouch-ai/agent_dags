736572e9ab44eef17caa77cbebb2caae9870f407
LMathew2001 <lmmathew@ecloudcontrol.com>
2025-08-20 17:18:10 +0530

    json for gmail credentials

--- autofinix-email-respond.py
+++ autofinix-email-respond.py
@@ -33,7 +33,7 @@ OLLAMA_HOST = "http://agentomatic:8000/"
 
 def authenticate_gmail():
     try:
-        creds = Credentials.from_authorized_user_info(GMAIL_CREDENTIALS)
+        creds = Credentials.from_authorized_user_info(json.loads(GMAIL_CREDENTIALS))
         service = build("gmail", "v1", credentials=creds)
         profile = service.users().getProfile(userId="me").execute()
         logged_in_email = profile.get("emailAddress", "")
