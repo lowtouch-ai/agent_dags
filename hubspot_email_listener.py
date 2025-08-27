c279597104d9ec38983cced462828595cb5ebd1a
LMathew2001 <lmmathew@ecloudcontrol.com>
2025-08-27 15:30:47 +0530

    Update hubspot_email_listener.py

--- hubspot_email_listener.py
+++ hubspot_email_listener.py
@@ -469,7 +469,7 @@ def trigger_meeting_minutes(**kwargs):
         task_id = f"trigger_response_{email['id'].replace('-', '_')}"
         trigger_task = TriggerDagRunOperator(
             task_id=task_id,
-            trigger_dag_id="hubspot_meeting_minutes_search",
+            trigger_dag_id="hubspot_search_entities",
             conf={"email_data": email},
         )
         trigger_task.execute(context=kwargs)
