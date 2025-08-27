976426f3226d3c8b2f0d5457576b31d367b6d715
LMathew2001 <lmmathew@ecloudcontrol.com>
2025-08-27 17:14:45 +0530

    dag name changes

--- hubspot_email_listener.py
+++ hubspot_email_listener.py
@@ -486,7 +486,7 @@ def trigger_meeting_minutes(**kwargs):
         task_id = f"trigger_response_{email['id'].replace('-', '_')}"
         trigger_task = TriggerDagRunOperator(
             task_id=task_id,
-            trigger_dag_id="hubspot_meeting_minutes_search",
+            trigger_dag_id="hubspot_search_entities",
             conf={"email_data": email},
         )
         trigger_task.execute(context=kwargs)
@@ -586,7 +586,7 @@ def trigger_continuation_dag(**kwargs):
         task_id = f"trigger_continuation_{email['id'].replace('-', '_')}"
         trigger_task = TriggerDagRunOperator(
             task_id=task_id,
-            trigger_dag_id="hubspot_meeting_minutes_continue",
+            trigger_dag_id="hubspot_create_entities",
             conf=continuation_conf,
         )
         trigger_task.execute(context=kwargs)
