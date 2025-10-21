ce291a7ea92b270964f3f90100b83139210fb233
Ajayrajc1998 <ajayrajc007@gmail.com>
2025-10-21 15:13:14 +0530

    Renamed the from address to Akamai-presales

--- akamai_send_email.py
+++ akamai_send_email.py
@@ -233,7 +233,7 @@ def send_email(service, recipient, subject, body, in_reply_to, references):
     try:
         logging.debug(f"Preparing email to {recipient} with subject: {subject}")
         msg = MIMEMultipart()
-        msg["From"] = f"CloudAssess via lowtouch.ai <{CLOUD_ASSESS_FROM_ADDRESS}>"
+        msg["From"] = f"Akamai-presales via lowtouch.ai <{CLOUD_ASSESS_FROM_ADDRESS}>"
         msg["To"] = recipient
         msg["Subject"] = subject
         msg["In-Reply-To"] = in_reply_to
